de3bf949d5b904f5ff387b4247c4d19a86f558f7
SpeNNy97 <solomatin.d.a97@gmail.com>
2020-07-13 21:42:18 +0300

    added example files

---
+++ HW1/src/Task2.java
@@ -0,0 +1,11 @@
+public class Task2 {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < args.length ; i++) {
+			
+			System.out.println(args[(args.length-1) - i]);
+			
+		}
+	}
+}
---
+++ HW1/src/Task3.java
@@ -0,0 +1,20 @@
+public class Task3 {
+	
+	public static void main(String[] args) {
+		
+		int[] arrayOfNums = { 4, 3, 7, 0, 9, 8 };
+		
+		for ( int arrayOfNum : arrayOfNums ) {
+			
+			System.out.println(arrayOfNum);
+		
+		}
+		
+		for ( int arrayOfNum : arrayOfNums ) {
+			
+			System.out.print(arrayOfNum + " ");
+			
+		}
+		
+	}
+}
