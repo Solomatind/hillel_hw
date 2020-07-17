7be4e236c39dc34cacbc58659cdb426668a32b51
SpeNNy97 <solomatin.d.a97@gmail.com>
2020-07-17 10:57:44 +0300

    Adding new files

---
+++ HW1/src/Task3.java
@@ -0,0 +1,20 @@
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        int[] arrayOfNums = {4, 3, 7, 0, 9, 8};
+
+        for (int num : arrayOfNums) {
+
+            System.out.println(num);
+
+        }
+
+        for (int num : arrayOfNums) {
+
+            System.out.print(num + " ");
+
+        }
+
+    }
+}
