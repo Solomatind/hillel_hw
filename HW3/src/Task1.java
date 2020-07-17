3e546eb8175d0bb43e40bff220d5c73c9e0f7a31
SpeNNy97 <solomatin.d.a97@gmail.com>
2020-07-17 11:16:05 +0300

    Adding new files

--- HW3/src/Task1.java
+++ HW3/src/Task1.java
@@ -1,32 +1,42 @@
+import java.util.*;
+
 public class Task1 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        int [] numbers = { 2, 3, 7, 15, 20, 76, 51, 93 };
-        int [] evenNumbers = new int [numbers.length] ;
-        int [] oddNumbers = new int [numbers.length];
+		int[] numbers = { 2, 3, 7, 15, 20, 76, 51, 93 };
+		//int[] evenNumbers = new int[numbers.length];
+		//int[] oddNumbers = new int[numbers.length];
+		ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
+		ArrayList<Integer> oddNumbers = new ArrayList<Integer>();
+		// for (int num : numbers) {
+		for (int i = 0; i < 8; i++) {
+			if (numbers[i] % 2 == 0) {
+				evenNumbers.add(numbers[i]);
+			} else {
+				//oddNumbers[i] = numbers[i];
+				oddNumbers.add(numbers[i]);
+			}
+		}
 
-        //for (int num : numbers) {
-			for (int i =0; i<8; i++) {
-            if (numbers[i] % 2 == 0) {
-                evenNumbers[i] = numbers[i];
-            }
-            else {
-                oddNumbers[i] = numbers[i];
-            }
-        }
+		System.out.print("Even numbers: ");
+		
+		for (int evenNumber : evenNumbers) {
+			System.out.print(evenNumber + " ");
+		}
+		/*for (int evenNum : evenNumbers) {
+			System.out.print(evenNum + " ");
+		}*/
 
-			 System.out.print("Even numbers: ");	
-        for (int evenNum : evenNumbers) {
-             System.out.print(evenNum + " ");
-        }
+		System.out.println("");
+		System.out.print("Odd numbers: ");
 
-        System.out.println("");
-        System.out.print("Odd numbers: ");
-        
-        for (int oddNum : oddNumbers) {
-        	
-             System.out.print(oddNum + " ");
+		for (int oddNumber : oddNumbers) {
+			System.out.print(oddNumber + " ");
+		}
+		/*for (int oddNum : oddNumbers) {
 
-    }
-}
+			System.out.print(oddNum + " ");
+
+		}*/
+	}
 }
