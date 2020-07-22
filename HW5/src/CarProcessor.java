eac76642d7fb2f83b2db432db1d41d08e7e3aebf
SpeNNy97 <solomatin.d.a97@gmail.com>
2020-07-22 20:01:17 +0300

    Adding HW5

---
+++ HW5/src/CarProcessor.java
@@ -0,0 +1,34 @@
+
+public class CarProcessor {
+
+	void screenByBrand(Car[] cars, String brand) {
+		for (Car car : cars) {
+			if (car.brand == brand) {
+				printCars(car);
+			}
+		}
+	}
+
+	void screenByModelAndExplPeriod(Car[] cars, String model, int year) {
+		for (Car car : cars) {
+			if (car.model == model && (2020 - car.year) > year) {
+				printCars(car);
+			}
+		}
+	}
+
+	void screenByYearAndCost(Car[] cars, int year, double cost) {
+		for (Car car : cars) {
+			if (car.year == year && car.cost > cost) {
+				printCars(car);
+			}
+		}
+	}
+
+	void printCars(Car car) {
+			System.out.println(car.id + " " + car.brand + " " +
+					car.model + " " + car.year + " " + car.color +
+					" " + car.cost + " " + car.registerNumber);
+	}
+
+}
